37756ea1b2d3aefdbd6448f5b722e0701ac160af
dudude <kirillinyakin@yandex.ru>
2021-06-08 14:16:22 +0300

    remove import

--- views/handle_map_markers.py
+++ views/handle_map_markers.py
@@ -1,6 +1,3 @@
-import ujson
-
-
 async def handle_get_map_markers(conn):
     query = "select place_uid, address, json_build_object('lat', lat, 'lng', lng)::json as location,  " \
             "place_name, place_type, icon, tag from map_markers"
